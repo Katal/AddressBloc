b6245da629b4e2f4040dd3c78b91e62df1531758
Lea <lea-artis@Leas-MacBook-Pro.local>
2015-09-14 11:00:54 -0700

    Assignments 21 and 22 are now both complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -106,28 +106,26 @@ RSpec.describe AddressBook do
 			expect(book_size).to eql 3
 		end
 
-		## COME BACK TO THIS AFTER NEXT CHECKPOINT
+		it "imports the 1st entry from csv 2, the sixth entry" do
+			book.import_from_csv("entries_2.csv")
+			#check first entry from book 2
+			entry_six = book.entries[0]
+			check_entry(entry_six, "Tamara", "702-555-5555", "tammy@blocmail.com")
+		end
 
-		# it "imports the 1st entry from csv 2, the sixth entry" do
-		# 	book.import_from_csv("entries_2.csv")
-		# 	#check first entry from book 2
-		# 	entry_six = book.entries[5]
-		# 	check_entry(entry_six, "Tamara", "702-555-5555", "tammy@blocmail.com")
-		# end
+		it "imports the 2nd entry from csv 2, the seventh entry in total" do
+			book.import_from_csv("entries_2.csv")
+			#check first entry from book 2
+			entry_seven = book.entries[1]
+			check_entry(entry_seven, "Ursula", "702-555-5551", "ursula@blocmail.com")
+		end
 
-		# it "imports the 2nd entry from csv 2, the seventh entry in total" do
-		# 	book.import_from_csv("entries_2.csv")
-		# 	#check first entry from book 2
-		# 	entry_seven = book.entries[6]
-		# 	check_entry(entry_seven, "Ursula", "702-555-5551", "ursula@blocmail.com")
-		# end
-
-		# it "imports the 3rd entry from csv 2, the eigth entry total" do
-		# 	book.import_from_csv("entries_2.csv")
-		# 	#check first entry from book 2
-		# 	entry_eight = book.entries[7]
-		# 	check_entry(entry_eight, "Zach", "750-555-5555", "zach@blocmail.com")
-		# end
+		it "imports the 3rd entry from csv 2, the eigth entry total" do
+			book.import_from_csv("entries_2.csv")
+			#check first entry from book 2
+			entry_eight = book.entries[2]
+			check_entry(entry_eight, "Zach", "750-555-5555", "zach@blocmail.com")
+		end
 
 	end
 
@@ -180,5 +178,54 @@ RSpec.describe AddressBook do
      end  
    end
 
+	describe "#iterative_search" do
+     it "searches AddressBook for a non-existent entry" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Dan")
+       expect(entry).to be_nil
+     end
+
+     it "searches AddressBook for Bill" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Bill")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Bill", "555-555-4854", "bill@blocmail.com")
+     end
+
+     it "searches AddressBook for Bob" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Bob")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+ 
+     it "searches AddressBook for Joe" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Joe")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
+ 
+     it "searches AddressBook for Sally" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Sally")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Sally", "555-555-4646", "sally@blocmail.com")
+     end
+ 
+     it "searches AddressBook for Sussie" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Sussie")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Sussie", "555-555-2036", "sussie@blocmail.com")
+     end
+
+     it "searches AddressBook for Billy" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Billy")
+       expect(entry).to be_nil
+     end 
+
+ 	end
 
 end
